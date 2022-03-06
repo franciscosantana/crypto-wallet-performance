6b967fb7510268e8798dd49d48805de2b0c48750
francisco.santana <casperbox@gmail.com>
2022-03-05 23:44:06 -0300

    refactor: Move result as string to the domain layer.

--- src/main/java/edu/chicoh/crypto/wallet/performance/CryptoWalletPerformanceApplication.java
+++ src/main/java/edu/chicoh/crypto/wallet/performance/CryptoWalletPerformanceApplication.java
@@ -1,6 +1,5 @@
 package edu.chicoh.crypto.wallet.performance;
 
-import edu.chicoh.crypto.wallet.performance.domain.model.CryptoAssetPosition;
 import edu.chicoh.crypto.wallet.performance.domain.model.CryptoWalletPerformance;
 import edu.chicoh.crypto.wallet.performance.infra.factory.AppFactory;
 import org.slf4j.Logger;
@@ -11,45 +10,21 @@ public class CryptoWalletPerformanceApplication {
     private static final Logger logger = LoggerFactory.getLogger(CryptoWalletPerformanceApplication.class);
 
     public static void main(String[] args) {
-        logger.info("Running Crypto Wallet Performance.");
+        try {
+            logger.info("Running Crypto Wallet Performance.");
 
-        final String defaultFileName = "wallet-sample.csv";
+            final String defaultFileName = "wallet-sample.csv";
 
-        final String result = AppFactory.getPerformanceAnalyzerService()
-                .analyze(defaultFileName)
-                .map(CryptoWalletPerformanceApplication::resultToString)
-                .orElse("Crypto wallet is empty.");
+            final String result = AppFactory.getPerformanceAnalyzerService()
+                    .analyze(defaultFileName)
+                    .map(CryptoWalletPerformance::resultToString)
+                    .orElse("Crypto wallet is empty.");
 
-        logger.info(result);
-        logger.info("Crypto wallet performance analyze completed.");
+            logger.info(result);
+            logger.info("Crypto wallet performance analyze completed.");
 
-        AppFactory.getExecutorService().shutdown();
-    }
-
-    private static String resultToString(CryptoWalletPerformance cryptoWalletPerformance) {
-        final StringBuilder description = new StringBuilder("Crypto wallet performance {");
-
-        description.append("total=").append(cryptoWalletPerformance.getTotal());
-
-        if (cryptoWalletPerformance.getBestPosition() != null) {
-            final CryptoAssetPosition bestPosition = cryptoWalletPerformance.getBestPosition();
-            description
-                    .append(", best_asset=")
-                    .append(bestPosition.getCryptoAsset().getSymbol())
-                    .append(", best_performance=")
-                    .append(bestPosition.getPerformance());
+        } finally {
+            AppFactory.getExecutorService().shutdown();
         }
-
-        if (cryptoWalletPerformance.getWorstPosition() != null) {
-            final CryptoAssetPosition worstPosition = cryptoWalletPerformance.getWorstPosition();
-            description
-                    .append(", worst_asset=")
-                    .append(worstPosition.getCryptoAsset().getSymbol())
-                    .append(", worst_performance=")
-                    .append(worstPosition.getPerformance());
-        }
-
-        description.append("}");
-        return description.toString();
     }
 }
--- src/main/java/edu/chicoh/crypto/wallet/performance/domain/model/CryptoWalletPerformance.java
+++ src/main/java/edu/chicoh/crypto/wallet/performance/domain/model/CryptoWalletPerformance.java
@@ -25,4 +25,31 @@ public class CryptoWalletPerformance {
     public CryptoAssetPosition getWorstPosition() {
         return worstPosition;
     }
+
+    public String resultToString() {
+        final StringBuilder description = new StringBuilder("Crypto wallet performance {");
+
+        description.append("total=").append(getTotal());
+
+        if (getBestPosition() != null) {
+            final CryptoAssetPosition bestPosition = getBestPosition();
+            description
+                    .append(", best_asset=")
+                    .append(bestPosition.getCryptoAsset().getSymbol())
+                    .append(", best_performance=")
+                    .append(bestPosition.getPerformance());
+        }
+
+        if (getWorstPosition() != null) {
+            final CryptoAssetPosition worstPosition = getWorstPosition();
+            description
+                    .append(", worst_asset=")
+                    .append(worstPosition.getCryptoAsset().getSymbol())
+                    .append(", worst_performance=")
+                    .append(worstPosition.getPerformance());
+        }
+
+        description.append("}");
+        return description.toString();
+    }
 }
